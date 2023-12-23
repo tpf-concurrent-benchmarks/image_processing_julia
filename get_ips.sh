b57f52e2e887075d4cc423358e976d6be616c8bf
efoppiano <efoppiano@fi.uba.ar>
2023-12-23 19:33:34 -0300

    Fix multiple bugs in docker compose and add ips_getter

---
+++ get_ips.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y dnsutils
+
+worker_types=("format" "resolution" "size")
+
+for worker_type in "${worker_types[@]}"; do
+    echo "Getting IPs for ${worker_type} workers"
+    counter=1
+    task_name="${worker_type}_worker"
+    for ip in $(nslookup tasks.${task_name} | awk '/^Address:/ && NR > 2 {print $2}'); do
+        echo "$ip" > "/ips/${worker_type}/ip_${counter}.txt"
+        ((counter++))
+    done
+done
